0e47621f4fafcef93ca6c79d82725ff28f8354e4
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-08-25 16:41:43 +0900

    feat : post 수정

--- stackoverflow/src/main/java/codestates/preproject/stackoverflow/post/entity/Posts.java
+++ stackoverflow/src/main/java/codestates/preproject/stackoverflow/post/entity/Posts.java
@@ -1,44 +1,53 @@
-//package codestates.preproject.stackoverflow.post.entity;
-//
-//
-//import codestates.preproject.stackoverflow.member.entity.Member;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//import java.time.LocalDateTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Entity
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Posts {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long postId;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "MEMBER_ID")
-//    private Member member;
-//
+package codestates.preproject.stackoverflow.post.entity;
+
+
+import codestates.preproject.stackoverflow.comments.entity.Comments;
+import codestates.preproject.stackoverflow.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long postId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @Column
+    private String subject;
+
 //    @Column
-//    private String subject;
-//
-//    @Column
-//    private List<String> tag;
-//
-//    @Column
-//    private String content;
-//
-//    @Column
-//    private int votes=0;
-//
-//    @Column(nullable = false)
-//    private LocalDateTime createdAt = LocalDateTime.now();
-//
-//
-//}
+//    private List<String> tag = new ArrayList<>();
+
+    @Column
+    private String content;
+
+    @Column
+    private int votes=0;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "posts")
+    List<Comments> commentsList = new ArrayList<>();
+
+    public void addComments(Comments comments){
+        this.commentsList.add(comments);
+        if(comments.getPosts() != this){
+            comments.setPosts(this);
+        }
+    }
+}

dbcff610b00bbb4264fd84fdce2784deb6bd16b6
shimeeuisuk <euisuk95@gmail.com>
2022-08-29 02:45:01 +0900

    Feat: 버튼 컴포넌트 변경 및 props 변경

--- client/src/Components/MainNav.jsx
+++ client/src/Components/MainNav.jsx
@@ -1,52 +1,72 @@
-import styled from "styled-components"
-import Button from "../Assets/Button"
-import stackoverflowlogo from "../Assets/Imgs/stackoverflowlogo.png"
+import styled from "styled-components";
+import Button from "../Assets/Button";
+import stackoverflowlogo from "../Assets/Imgs/stackoverflowlogo.png";
+import { useNavigate } from "react-router-dom";
+import ColorButton from "../Assets/BlueBtn";
 
-function MainNav () {
-return(<Container><Line/>
-<Navbody>
-    <img src={stackoverflowlogo} className="logo" alt="react"/>
-    <div className="btnwrapper">
-    <Button padding={8} bgcolor="#E1ECF4" color="#39739D" ftsize={13} text="Log in" bordercolor="#7aa7c7" hovercolor="#b3d3ea"/>
-    <Button className="signupbtn" padding={8} bgcolor="#0995ff" color="#FFFFFF" ftsize={13} text="Sign up" bordercolor="#0995ff" hovercolor="#0074cc"/>
-    </div>
-</Navbody>
-
-</Container>)
+function MainNav() {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Line />
+      <Navbody>
+        <img src={stackoverflowlogo} className="logo" alt="react" />
+        <div className="btnwrapper">
+          <ColorButton
+            mode="GREY"
+            text="Log in"
+            onClick={() => {
+              navigate("/login");
+            }}
+          />
+          <ColorButton
+            mode="BLUE"
+            text="Sign up"
+            onClick={() => {
+              navigate("/signup");
+            }}
+          />
+        </div>
+      </Navbody>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    width: 100%;
-    height: 50px;
-    background-color: #f8f9f9;
-    box-shadow: 0 1px 2px lightgray;
-`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  background-color: #f8f9f9;
+  box-shadow: 0 1px 2px lightgray;
+`;
 const Line = styled.div`
-    width: 100%;
-    height: 3px;
-    background-color: #f48225;
-`
+  width: 100%;
+  height: 3px;
+  background-color: #f48225;
+`;
 const Navbody = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 47px;
+  .btnwrapper {
     display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width:100%;
-    height: 47px;
-    .btnwrapper{
-        display:flex;
-        margin-right: 20px;
-    }
-    .loginpbtn{
-       margin-right: 10px;
-    }
-    .logo{
-        width:158px;
-        height: 30px;
-        margin-left: 56px;
-    }
-    .signupbtn{
-        margin-left: 10px;
-    }
-`
+    margin-right: 20px;
+  }
+  .btnwrapper > button {
+    margin-left: 10px;
+  }
+  .loginpbtn {
+    margin-right: 10px;
+  }
+  .logo {
+    width: 158px;
+    height: 30px;
+    margin-left: 56px;
+  }
+`;
 
-export default MainNav
+export default MainNav;

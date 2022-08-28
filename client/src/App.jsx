acdf6b0371d2a53987975b998f3a76729d4fa7c7
shimeeuisuk <euisuk95@gmail.com>
2022-08-29 02:40:15 +0900

    Feat: 라우터 기능 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import HomePage from "./Pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import QuestionPage from "./Pages/QuestionPage";
+import MainPage from "./Pages/MainPage";
+import SignUp from "./Components/SignUp";
+import LogIn from "./Components/LogIn";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />}></Route>
+      <Route path="/home" element={<HomePage />}></Route>
+      <Route path="/questions" element={<QuestionPage />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/login" element={<LogIn />}></Route>
+    </Routes>
+  );
 }
 
 export default App;

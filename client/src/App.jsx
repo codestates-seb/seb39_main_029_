fec40dc51c6afe5974a5aa40971e1e6d04cf930c
tahupapa <xognl3164@gmail.com>
2022-08-29 10:03:57 +0900

    🎨 라우트 변경

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,17 @@ import HomePage from "./Pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import QuestionPage from "./Pages/QuestionPage";
 import MainPage from "./Pages/MainPage";
-import SignUp from "./Components/SignUp";
-import LogIn from "./Components/LogIn";
+import SignUpPage from "./Pages/SignupPage";
+import LogInPage from "./Pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />}></Route>
+      <Route path="/signup" element={<SignUpPage />}></Route>
+      <Route path="/login" element={<LogInPage />}></Route>
       <Route path="/home" element={<HomePage />}></Route>
       <Route path="/questions" element={<QuestionPage />}></Route>
-      <Route path="/signup" element={<SignUp />}></Route>
-      <Route path="/login" element={<LogIn />}></Route>
     </Routes>
   );
 }

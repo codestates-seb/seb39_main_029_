135be4840805e01c39086fd4141cae90a760fcca
tahupapa <xognl3164@gmail.com>
2022-08-29 10:03:42 +0900

    📝 로그인/회원가입 페이지 추가

---
+++ client/src/Pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import MainNav from "../Components/MainNav";
+import LogIn from "../Components/LogIn";
+
+function LogInPage() {
+  return (
+    <>
+      <MainNav />
+      <LogIn />
+    </>
+  );
+}
+
+export default LogInPage;
---
+++ client/src/Pages/SignupPage.jsx
@@ -0,0 +1,13 @@
+import MainNav from "../Components/MainNav";
+import SignUp from "../Components/SignUp";
+
+function SignUpPage() {
+  return (
+    <>
+      <MainNav />
+      <SignUp />
+    </>
+  );
+}
+
+export default SignUpPage;

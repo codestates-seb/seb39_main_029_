12dee0ed7b0d9689e7f858db87f3d8f442ebb410
jungdoyoon <jungdo8016@gmail.com>
2022-08-29 16:27:47 +0900

    Fix : Post 수정

--- stackoverflow/src/main/java/codestates/preproject/stackoverflow/comments/entity/Comments.java
+++ stackoverflow/src/main/java/codestates/preproject/stackoverflow/comments/entity/Comments.java
@@ -40,9 +40,9 @@ public class Comments {
 
     public void setPosts(Posts posts) {
         this.posts = posts;
-        if(!posts.getCommentsList().contains(this)){
+        /*if(!posts.getCommentsList().contains(this)){
             posts.addComments(this);
-        }
+        }*/
     }
 
     public void addlist(Cvote cvote){

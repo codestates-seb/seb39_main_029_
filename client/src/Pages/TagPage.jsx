36021fb81efc359d750702e7de4eb258ddf0b262
tahupapa <xognl3164@gmail.com>
2022-08-30 15:30:32 +0900

    ✨ 태그 페이지 추가

---
+++ client/src/Pages/TagPage.jsx
@@ -0,0 +1,22 @@
+import Nav from "../Components/Nav";
+import Sidebar from "../Components/Sidebar";
+import Tags from "../Components/Tags";
+import styled from "styled-components";
+
+function TagPage() {
+  return (
+    <>
+      <Nav />
+      <Wrapper>
+        <Sidebar />
+        <Tags />
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+export default TagPage;

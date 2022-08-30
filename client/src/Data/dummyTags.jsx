53ea6db708749d6bf96b7b7a9c8b57ffe0ae9044
tahupapa <xognl3164@gmail.com>
2022-08-30 15:28:56 +0900

    🗃️ 더미데이터 추가

---
+++ client/src/Data/dummyTags.jsx
@@ -0,0 +1,32 @@
+export const DummyTag = [
+  {
+    id: 1,
+    tag: "HTTP",
+    exp: "Hypertext Transfer Protocol (HTTP) is an application level network protocol that is used for the transfer of content on the World Wide Web.",
+  },
+  {
+    id: 2,
+    tag: "CSS",
+    exp: "CSS (Cascading Style Sheets) is a representation style sheet language used for describing the look and formatting of HTML (HyperText Markup Language), XML (Extensible Markup Language) documents and SVG elements including (but not limited to) colors, layout, fonts, and animations. It also describes how elements should be rendered on screen, on paper, in speech, or on other media.",
+  },
+  {
+    id: 3,
+    tag: "JAVASCRIPT",
+    exp: "For questions regarding programming in ECMAScript (JavaScript/JS) and its various dialects/implementations (excluding ActionScript). Note JavaScript is NOT the same as Java! Please include all relevant tags on your question; e.g., [node.js], [jquery], [json], [reactjs], [angular], [ember.js], [vue.js], [typescript], [svelte], etc.",
+  },
+  {
+    id: 4,
+    tag: "REACT",
+    exp: "React is a JavaScript library for building user interfaces. It uses a declarative, component-based paradigm and aims to be both efficient and flexible.",
+  },
+  {
+    id: 5,
+    tag: "REDUX",
+    exp: "Redux is a pattern and library for managing JavaScript application state, using events called actions. It serves as a centralized store for state that is needed across your entire application, with rules ensuring that the state can only be updated in a predictable fashion. Redux make it easier to understand when, where, why, and how the state in your application is being updated, and how your application logic will behave when those changes occur.",
+  },
+  {
+    id: 5,
+    tag: "RECOIL",
+    exp: "A state management library for React.",
+  },
+];

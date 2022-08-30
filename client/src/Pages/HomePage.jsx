e5e14b03542317123dfb641718857aabfc4a37f4
shimeeuisuk <euisuk95@gmail.com>
2022-08-30 16:08:31 +0900

    🎨 Feat: 게시글 목록 데이터 요청 기능 추가

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -3,30 +3,51 @@ import Nav from "../Components/Nav";
 import Sidebar from "../Components/Sidebar";
 import PostBox from "../Components/PostBox";
 import Button from "../Assets/Button";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const [totalPosts, setTotalPosts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://ec2-15-165-204-159.ap-northeast-2.compute.amazonaws.com:8080/v1/posts?page=1&size=10&arrange=createdAt&tagCheckId=-1"
+      )
+      .then((res) => {
+        setTotalPosts(res.data.data);
+      });
+  }, []);
+
   return (
     <Container>
       <Nav />
-      <Sidebar />
-      <Body>
-        <Title>
-          <Titletop>
-            <span className="top">Top Questions</span>
-            <Button
-              text={"Ask Question"}
-              bgcolor="var(--theme-blue)"
-              padding={10}
-              color="var(--font-color-white)"
-              ftsize={13}
-            />
-          </Titletop>
-          <Titlebottom></Titlebottom>
-        </Title>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => (
-          <PostBox />
-        ))}
-      </Body>
+      <Wrapper>
+        <Sidebar />
+        <Body>
+          <Title>
+            <Titletop>
+              <span className="top">Top Questions</span>
+              <Button
+                text={"Ask Question"}
+                bgcolor="var(--theme-blue)"
+                padding={10}
+                color="var(--font-color-white)"
+                ftsize={13}
+                onClick={() => {
+                  navigate("/ask");
+                }}
+              />
+            </Titletop>
+            <Titlebottom></Titlebottom>
+          </Title>
+          {totalPosts.map((el, idx) => (
+            <PostBox key={idx} post={el} />
+          ))}
+        </Body>
+      </Wrapper>
     </Container>
   );
 }
@@ -35,20 +56,23 @@ const Container = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  width: auto;
+  width: 100%;
   height: auto;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 const Body = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 50px;
-  margin-left: 240px;
-  width: 1232px;
 `;
 
 const Title = styled.div`
-  width: 1032px;
+  width: 100%;
   height: 130px;
 `;
 const Titletop = styled.div`

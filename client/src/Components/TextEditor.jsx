0b302bec1eed4f5dec389b7808cb8f49926f726f
shimeeuisuk <euisuk95@gmail.com>
2022-08-30 16:07:41 +0900

    💄 Add: toast text editor ui 추가

---
+++ client/src/Components/TextEditor.jsx
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+
+// Toast 에디터
+import { Editor } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
+import "tui-color-picker/dist/tui-color-picker.css";
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+import "@toast-ui/editor/dist/i18n/ko-kr";
+
+function TextEditor() {
+  // Editor DOM 선택용
+  const editorRef = useRef();
+
+  // 등록 버튼 핸들러
+  const handleRegisterButton = () => {
+    // 입력창에 입력한 내용을 HTML 태그 형태로 취득
+    console.log(editorRef.current?.getInstance().getHTML());
+    // 입력창에 입력한 내용을 MarkDown 형태로 취득
+    console.log(editorRef.current?.getInstance().getMarkdown());
+  };
+
+  return (
+    <div>
+      <Editor
+        ref={editorRef} // DOM 선택용 useRef
+        placeholder="내용을 입력해주세요."
+        previewStyle="vertical" // 미리보기 스타일 지정
+        height="280px" // 에디터 창 높이
+        initialEditType="wysiwyg" //
+        toolbarItems={[
+          // 툴바 옵션 설정
+          ["heading", "bold", "italic", "strike"],
+          ["hr", "quote"],
+          ["ul", "ol", "task", "indent", "outdent"],
+          ["table", "image", "link"],
+          ["code", "codeblock"],
+        ]}
+        useCommandShortcut={false} // 키보드 입력 컨트롤 방지
+        language="ko-KR"
+        plugins={[colorSyntax]}
+      ></Editor>
+
+      <button onClick={handleRegisterButton}>등록</button>
+    </div>
+  );
+}
+
+export default TextEditor;

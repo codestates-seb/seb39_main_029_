2db4eb7de0613893b1a90e302ebb6cec40960af6
shimeeuisuk <euisuk95@gmail.com>
2022-08-30 16:01:30 +0900

    🌱 Add: recoil atom 추가

---
+++ client/src/States/LoginState.jsx
@@ -0,0 +1,10 @@
+import { atom } from "recoil";
+// import { recoilPersist } from "recoil-persist";
+
+// const { persistAtom } = recoilPersist(); // 페이지 이동 후에도 상태 유지
+
+export const LoginState = atom({
+  key: "LoginState",
+  default: false,
+  // effects_UNSTABLE: [persistAtom],
+});

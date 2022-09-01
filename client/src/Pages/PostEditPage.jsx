acaa7238e4e00360a55746baec05fa1595563119
shimeeuisuk <euisuk95@gmail.com>
2022-09-01 17:28:25 +0900

    🎨 Feat: 게시글 post 및 patch  기능 추가

--- client/src/Pages/PostEditPage.jsx
+++ client/src/Pages/PostEditPage.jsx
@@ -2,16 +2,40 @@ import styled from "styled-components";
 import Nav from "../Components/Nav";
 import AskBody from "../Components/AskBody";
 import ColorButton from "../Assets/ColorBtn";
+import Sidebar from "../Components/Sidebar";
+import axios from "axios";
 
 function PostEditPage() {
+  const token = localStorage.getItem("accessToken");
+  console.log(token);
+
+  const editForm = {
+    subject: "How I send img to server?",
+    memberId: 2,
+    content:
+      "send img to server? I have two method but i don't know what is best. First, i can use base 64 or formData. What is best?",
+    postTag: [{ tagId: 1 }, { tagId: 2 }],
+  };
+
+  const Edit = () => {
+    axios
+      .patch("http://seb039pre029.ga:8080/v1/posts/1", editForm, {
+        headers: { Authorization: token },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
     <Container>
       <Nav />
       <Wrapper>
+        <Sidebar />
         <AskBody />
       </Wrapper>
       <ButtonWrapper>
-        <ColorButton mode="BLUE" text="Save Edit"></ColorButton>
+        <ColorButton mode="BLUE" text="Save Edit" onClick={Edit}></ColorButton>
       </ButtonWrapper>
     </Container>
   );
@@ -21,16 +45,12 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: var(--theme-selected-grey);
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100vmax;
-  height: 100vmax;
+  background-color: var(--theme-white);
 `;
 
 const Wrapper = styled.div`
-  margin-top: 150px;
+  display: flex;
+  height: 100vmax;
 `;
 
 const ButtonWrapper = styled.div`
--- client/src/Pages/PostWritePage.jsx
+++ client/src/Pages/PostWritePage.jsx
@@ -3,24 +3,84 @@ import Nav from "../Components/Nav";
 import AskBody from "../Components/AskBody";
 import ArcodianBox from "../Components/ArcodianBox";
 import ColorButton from "../Assets/ColorBtn";
-import { useState } from "react";
+import TagModal from "../Components/TagModal";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useRef } from "react";
 
 function PostWritePage() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [tag, setTag] = useState([]);
+  const [selectedTag, setSelectedTag] = useState([]);
+  const [autoSeleted, setAutoSelected] = useState([]);
+  const user = localStorage.getItem("memberId");
+  const token = localStorage.getItem("accessToken");
+  const [totalTags, setTotalTags] = useState([]);
+  const editorRef = useRef();
+  console.log(totalTags);
+  console.log(selectedTag);
+
+  useEffect(() => {
+    axios
+      .get("http://seb039pre029.ga:8080/v1/tags/list", {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        setTotalTags(res.data);
+      });
+  }, []);
+
+  const Postform = {
+    subject: "How I send img to server?",
+    memberId: 2,
+    content:
+      "How I send img to server? I have two method but i don't know what is best. First, i can use base 64 or formData. What is best?",
+    postTag: [{ tagId: 1 }, { tagId: 2 }],
+  };
+
+  const Post = () => {
+    console.log(token);
+    axios
+      .post("http://seb039pre029.ga:8080/v1/posts", Postform, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
 
   return (
     <Container>
       <Nav />
       <Title>Ask a Public question</Title>
       <Wrapper>
-        <AskBody />
+        <AskBody
+          setTitle={setTitle}
+          setContent={setContent}
+          totalTags={totalTags}
+          setAutoSelected={setAutoSelected}
+          editorRef={editorRef}
+        />
         <ArcodianBox />
       </Wrapper>
       <ButtonWrapper>
-        <ColorButton mode="BLUE" text="Review your question"></ColorButton>
+        <ColorButton
+          mode="BLUE"
+          text="Review your question"
+          onClick={Post}
+        ></ColorButton>
       </ButtonWrapper>
+      <TagModal
+        token={token}
+        autoSeleted={autoSeleted}
+        totalTags={totalTags}
+        selectedTag={selectedTag}
+        setSelectedTag={setSelectedTag}
+      />
     </Container>
   );
 }
@@ -33,11 +93,11 @@ const Container = styled.div`
   background-color: var(--theme-selected-grey);
 `;
 const Title = styled.div`
+  font-weight: bold;
+  padding: 0 0 0 40px;
   display: flex;
   margin-top: 30px;
   font-size: var(--header-font);
-  font-weight: bold;
-  padding: 0 0 0 40px;
 `;
 
 const Wrapper = styled.div`

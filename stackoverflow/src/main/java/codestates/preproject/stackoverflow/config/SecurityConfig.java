7a0e47f379b2b76631c6ea2b5ed1cabf45d2e617
jungdoyoon <jungdo8016@gmail.com>
2022-09-02 11:07:47 +0900

    Test : RDS

--- stackoverflow/src/main/java/codestates/preproject/stackoverflow/config/SecurityConfig.java
+++ stackoverflow/src/main/java/codestates/preproject/stackoverflow/config/SecurityConfig.java
@@ -2,26 +2,24 @@ package codestates.preproject.stackoverflow.config;
 
 import codestates.preproject.stackoverflow.filter.JwtAuthenticationFilter;
 import codestates.preproject.stackoverflow.filter.JwtAuthorizationFilter;
-//import codestates.preproject.stackoverflow.filter.testfilter;
 import codestates.preproject.stackoverflow.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-//@RequiredArgsConstructor
 public class SecurityConfig {
     @Autowired
     private CorsFilter corsFilter;
@@ -32,7 +30,8 @@ public class SecurityConfig {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
+    @Autowired
+    private HttpSecurity httpSecurity;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{

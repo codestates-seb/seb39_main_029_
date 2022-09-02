6a62aa45897e05329b9303ca8877ccf70d29fc12
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-02 11:59:29 +0900

    rds : test

--- stackoverflow/src/main/java/codestates/preproject/stackoverflow/StackoverflowApplication.java
+++ stackoverflow/src/main/java/codestates/preproject/stackoverflow/StackoverflowApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"'org.springframework.security.config.annotation.web.builders.HttpSecurity"})
 public class StackoverflowApplication {
 
 	@Bean

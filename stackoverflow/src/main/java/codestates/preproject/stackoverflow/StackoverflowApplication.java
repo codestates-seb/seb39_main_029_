4efa797b4deb82dc7f1ec5859764784b9e5f779e
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-02 15:51:11 +0900

    test

--- stackoverflow/src/main/java/codestates/preproject/stackoverflow/StackoverflowApplication.java
+++ stackoverflow/src/main/java/codestates/preproject/stackoverflow/StackoverflowApplication.java
@@ -2,11 +2,12 @@ package codestates.preproject.stackoverflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication(scanBasePackages = {"org.springframework.security.config.annotation.web.builders.HttpSecurity"})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class StackoverflowApplication {
 
 	@Bean
